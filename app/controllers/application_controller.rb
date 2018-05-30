8f150840034e06a85bd62ffeb2ead4d6b475ea75
raco82 <mail.racorp@gmail.com>
2018-05-30 19:48:37 +0200

    stable

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  before_action :sanitize_devise_params, if: :devise_controller?
-
   def sanitize_devise_params
     devise_parameter_sanitizer.permit(:sign_up) { |user| user.permit(
     :username,
