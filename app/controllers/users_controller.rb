468be2d5f9600eab7db665c15637be9e473442bb
raco82 <mail.racorp@gmail.com>
2018-05-31 19:29:55 +0200

    0

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,9 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @orders = Order.where(params[:id])
   end
 
   def admin
     @user = User.find(params[:admin])
-  end
-
-  def create
-
   end
 end
